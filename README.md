820204329f0519e0f778c5ce68547b34dc44366b
jhonjson <18101905140@163.com>
2018-05-18 15:31:53 +0800

    fix

--- README.md
+++ README.md
@@ -1,2 +1,75 @@
 # BottomListDiaLogs
-统一dialog  底部弹出框
+封装通用dialog  底部弹出框
+
+由于每次写dialog都要写很多重复代码，因此简单的封装了一个BottomListDiaLogs,封装了一些常用方法，使用Builder模式实现:
+
+### 展示效果：
+![效果图1](img/9FD09CD2-8D55-4528-9870-9547880BBDC6.png)
+
+![效果图2](img/50C6C998-A2DE-4E18-B524-3D5D15F88EF9.png)
+
+### 使用方式：
+
+Add the dependency to your build.gradle.
+
+```java
+dependencies {
+    implementation 'com.github.Johnjson:BottomListDiaLogs:v1.0.0'
+}
+```
+
+
+### 更新日志
+
+**v1.0.0：**
+
+```  public void showBottomListDialog1(@final Context context, String[] mList) {
+        new BottomListDialog.Builder(context).
+                addMenuListItem(mList, new OnClickPositionListener() {
+                    @Override
+                    public void onClickPosition(int position) {
+                        Toast.makeText(context, "点击第几个位置    " + position, Toast.LENGTH_LONG).show();
+                    }
+                }).show();
+    }
+```
+
+```public void showBottomListDialog2( final Context context, int color, int size) {
+        new BottomListDialog.Builder(context).
+                addMenuItem(new BottomListDialog.BottomListMenuItem("张三", new OnClickPositionListener() {
+                    @Override
+                    public void onClickPosition(int position) {
+                        Toast.makeText(context, "点击第几个位置    " + position, Toast.LENGTH_LONG).show();
+                    }
+                },color,size)).
+                addMenuItem(new BottomListDialog.BottomListMenuItem("李四", new OnClickPositionListener() {
+                    @Override
+                    public void onClickPosition(int position) {
+                        Toast.makeText(context, "点击第几个位置    " + position, Toast.LENGTH_LONG).show();
+                    }
+                },color,size)).
+                show();
+    }
+```
+
+```    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+            /**底部弹出*/
+            case R.id.mbottom1:
+                String[] mList = MainActivity.this.getResources().getStringArray(R.array.selectList);
+                showBottomListDialog1(MainActivity.this,mList);
+                break;
+
+            case R.id.mbottom2:
+                showBottomListDialog2(MainActivity.this,R.dimen.com_font_size_16,getResources().getColor(R.color.colorAccent));
+                break;
+            default:
+                break;
+        }
+
+    }
+```
+
+如果你喜欢，欢迎stars
--- app/src/main/java/com/example/jhonjson/bottomlistdialogs/MainActivity.java
+++ app/src/main/java/com/example/jhonjson/bottomlistdialogs/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.jhonjson.bottomlistdialogs;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -15,7 +16,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     /**
      * 底部弹出
      */
-    private TextView mbottom;
+    private TextView mbottom1,mbottom2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,8 +26,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void initView() {
-        mbottom = (TextView) findViewById(R.id.mbottom);
-        mbottom.setOnClickListener(this);
+        mbottom1 = (TextView) findViewById(R.id.mbottom1);
+        mbottom2 = (TextView) findViewById(R.id.mbottom2);
+        mbottom1.setOnClickListener(this);
+        mbottom2.setOnClickListener(this);
     }
 
 
@@ -35,8 +38,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         switch (v.getId()) {
             /**底部弹出*/
-            case R.id.mbottom:
-                showBottomListDialog();
+            case R.id.mbottom1:
+                String[] mList = MainActivity.this.getResources().getStringArray(R.array.selectList);
+                showBottomListDialog1(MainActivity.this,mList);
+                break;
+
+            case R.id.mbottom2:
+                showBottomListDialog2(MainActivity.this,R.dimen.com_font_size_16,getResources().getColor(R.color.colorAccent));
                 break;
             default:
                 break;
@@ -45,15 +53,33 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
 
-    private void showBottomListDialog() {
-        String[] mReportList = MainActivity.this.getResources().getStringArray(R.array.selectList);
-        new BottomListDialog.Builder(MainActivity.this).
-                addMenuItem(new BottomListDialog.BottomListMenuItem("张三的锅")).
-                addMenuListItem(mReportList, new OnClickPositionListener() {
+    public void showBottomListDialog1(final Context context, String[] mList) {
+        new BottomListDialog.Builder(context).
+                addMenuListItem(mList, new OnClickPositionListener() {
                     @Override
                     public void onClickPosition(int position) {
-                        Toast.makeText(MainActivity.this, "点击第几个位置    " + (position - 1), Toast.LENGTH_LONG).show();
+                        Toast.makeText(context, "点击第几个位置    " + position, Toast.LENGTH_LONG).show();
                     }
                 }).show();
     }
+
+
+    public void showBottomListDialog2( final Context context, int color, int size) {
+        new BottomListDialog.Builder(context).
+                addMenuItem(new BottomListDialog.BottomListMenuItem("张三", new OnClickPositionListener() {
+                    @Override
+                    public void onClickPosition(int position) {
+                        Toast.makeText(context, "点击第几个位置    " + position, Toast.LENGTH_LONG).show();
+                    }
+                },color,size)).
+                addMenuItem(new BottomListDialog.BottomListMenuItem("李四", new OnClickPositionListener() {
+                    @Override
+                    public void onClickPosition(int position) {
+                        Toast.makeText(context, "点击第几个位置    " + position, Toast.LENGTH_LONG).show();
+                    }
+                },color,size)).
+                show();
+    }
+
+
 }
