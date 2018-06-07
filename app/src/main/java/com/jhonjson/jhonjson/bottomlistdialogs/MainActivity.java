ef1a30915fd45c789922fa6a170ec00c78fe269f
jhonjson <18101905140@163.com>
2018-06-07 17:20:10 +0800

    添加选择弹出对话框

--- app/src/main/java/com/jhonjson/jhonjson/bottomlistdialogs/MainActivity.java
+++ app/src/main/java/com/jhonjson/jhonjson/bottomlistdialogs/MainActivity.java
@@ -3,11 +3,13 @@ package com.jhonjson.jhonjson.bottomlistdialogs;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.jhonjson.dialoglib.BottomListDialog;
+import com.jhonjson.dialoglib.CommonSelectDialog;
 import com.jhonjson.dialoglib.MiddleListDialog;
 import com.jhonjson.dialoglib.interfaces.OnClickPositionListener;
 
@@ -27,6 +29,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         findViewById(R.id.middle1).setOnClickListener(this);
         findViewById(R.id.middle2).setOnClickListener(this);
+        findViewById(R.id.select).setOnClickListener(this);
+        findViewById(R.id.selec1).setOnClickListener(this);
     }
 
 
@@ -52,6 +56,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             /**中部弹出2*/
             case R.id.middle2:
                 showMiddleListDialog2(MainActivity.this, R.dimen.com_font_size_16, getResources().getColor(R.color.colorAccent));
+                break;
+
+            /**选择弹出框*/
+            case R.id.select:
+                showSelectDialog(MainActivity.this, "提示", "你确定要关闭这个dialog吗？", "取消", "确认", 16);
+                break;
+            /**单个按钮弹出框*/
+            case R.id.selec1:
+                showSelectOneDialog(MainActivity.this, "提示", "你确定要删除这个dialog吗？", "确认", 16);
                 break;
             default:
                 break;
@@ -111,13 +124,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
      */
     public void showMiddleListDialog2(final Context context, int color, int size) {
         new MiddleListDialog.Builder(context).
-                addMenuItem(new MiddleListDialog. MiddleListMenuItem("张三", new OnClickPositionListener() {
+                addMenuItem(new MiddleListDialog.MiddleListMenuItem("张三", new OnClickPositionListener() {
                     @Override
                     public void onClickPosition(int position) {
                         Toast.makeText(context, "点击第几个位置    " + position, Toast.LENGTH_LONG).show();
                     }
                 }, color, size)).
-                addMenuItem(new MiddleListDialog. MiddleListMenuItem("李四", new OnClickPositionListener() {
+                addMenuItem(new MiddleListDialog.MiddleListMenuItem("李四", new OnClickPositionListener() {
                     @Override
                     public void onClickPosition(int position) {
                         Toast.makeText(context, "点击第几个位置    " + position, Toast.LENGTH_LONG).show();
@@ -126,5 +139,50 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 show();
     }
 
+    /**
+     * 选择弹出框
+     */
+    CommonSelectDialog mCommonSelectDialog = null;
+
+    public void showSelectDialog(final Context context, String mTitle, String mContent, String cancel, String confirm, int size) {
+        mCommonSelectDialog = new CommonSelectDialog.Builder(context).
+                setContentGravity(Gravity.CENTER).
+                setTouchAble(true).
+                setTitle(mTitle).
+                setContent(mContent).
+                setContentSize(size).
+                setLeftButtonInterface(cancel, new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        mCommonSelectDialog.dismiss();
+                    }
+                }).setRightButtonInterface(confirm, new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCommonSelectDialog.dismiss();
+            }
+        }).show();
+
+    }
+
+
+    /**
+     * 单个按钮弹出框
+     */
+    public void showSelectOneDialog(final Context context, String mTitle, String mContent, String confirm, int size) {
+        mCommonSelectDialog = new CommonSelectDialog.Builder(context).
+                setContentGravity(Gravity.CENTER).
+                setTouchAble(true).
+                setTitle(mTitle).
+                setContent(mContent).
+                setContentSize(size).
+                setOneButtonInterface(confirm, new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        mCommonSelectDialog.dismiss();
+                    }
+                }).show();
+
+    }
 
 }
